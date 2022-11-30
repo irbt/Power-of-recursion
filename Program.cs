3ed04247138b8e57544ea5ff335162c1e94db702
Emmanuel Marchal <81331382+irbt@users.noreply.github.com>
2022-11-30 11:04:02 +0100

    final

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// Given two integers, write a method that returns first number raised to the power of second number.
+
+
+namespace Program{
+    class PowerRecursionTask{
+        public static void Main(string [] args){
+            Console.Clear();
+            static long ToThePowerOfRecursion(int num , int exp){
+                
+                 return (exp == 0 ) ? 1 : num * ToThePowerOfRecursion(num, exp-1);
+            }
+            Console.WriteLine(ToThePowerOfRecursion(18, 6));// → 8
+            Console.WriteLine(ToThePowerOfRecursion(5, 2));// → 25
+        }
+    }
+}
